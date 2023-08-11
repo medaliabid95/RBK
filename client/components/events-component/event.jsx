d140a95ba564279cdc2a9f73e588459b99d10427
MoNourTrabelsi <trabelsimednour@gmail.com>
2023-08-11 18:46:31 +0100

    event fully done

--- client/components/events-component/event.jsx
+++ client/components/events-component/event.jsx
@@ -1,11 +1,12 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
 import axios from 'axios'
+import Link from 'next/link'
 import "./event.css"
 export const Event = () => {
     const [events, setEvents] = useState([])
 
+    
     useEffect(() => {
         fetch()
     }, [])
@@ -16,21 +17,21 @@ export const Event = () => {
             .catch((err) => console.log(err))
     }
 
-    const router = useRouter()
     return (
         <div>
             {events.map((event) => (
                 <div className='one-event'>
                     <div className='event-date'>
                         <img src="dateicon.png" alt="" />
-                        <h3>{event.date}</h3>
+                        <h3>{event.heure}</h3>
                     </div>
                     <h1 className='event-title'>{event.title}</h1>
                     <div className='event-paragraph truncate'>{event.description}</div>
-                    <button onClick={() => {
-                        router.push(`/events/${event.id}`)
-                    }} className='read-more'>read more</button>
-                    <img className='event-img' src="0.jpg" alt="" />
+                    <Link className='read-more' href={`/events/${event.id}`}>
+                   <span className='read-more-text'>read more</span>
+                    </Link>
+                    
+                    <img className='event-img' src={event.image} alt="" />
                     <div className='line-after'></div>
                 </div>
             ))}
