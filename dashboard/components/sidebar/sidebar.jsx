41df76a6d9c96682c853444c1cbb226f571ebbc0
medaliabid <dali.abid2014@gmail.com>
2023-08-14 10:43:09 +0100

    monday-11h

--- dashboard/components/sidebar/sidebar.jsx
+++ dashboard/components/sidebar/sidebar.jsx
@@ -18,13 +18,13 @@ const Sidebar = () => {
 
     return (
         <header className='header-sidebar'>
-            <a href="/"><img src="/RBK-Logo.svg" alt="RBK-LOGO" className='logo-nav' /></a>
+            <a href="/newStudents"><img src="/RBK-Logo.svg" alt="RBK-LOGO" className='logo-nav' /></a>
             <div className='location'>
                 <FontAwesomeIcon icon={faLocationDot} id='location-dot' style={{ color: "#FF007B", width: "17px" }} />
                 <span id='location-sidebard'>{location}</span>
             </div>
             <ul className='side-links'>
-                <li className='side-link'><span><FontAwesomeIcon icon={faBorderAll} style={{ width: "21px" }} /></span><a href="/" className='side-anchor-link'>Tous le demandes</a></li>
+                <li className='side-link'><span><FontAwesomeIcon icon={faBorderAll} style={{ width: "21px" }} /></span><a href="/allStudents" className='side-anchor-link'>Tous le demandes</a></li>
                 <li className='side-link'><span><FontAwesomeIcon icon={faInbox} style={{ width: "21px" }} /></span><a href="/" className='side-anchor-link'>Tous le messages</a></li>
                 <li className='side-link'><span><FontAwesomeIcon icon={faPeopleGroup} style={{ width: "21px" }} /></span><a href="/" className='side-anchor-link'>Cohorts</a></li>
                 <li className='side-link'><span><FontAwesomeIcon icon={faFeather} style={{ width: "21px" }} /></span><a href="/" className='side-anchor-link'>Blogs</a></li>
