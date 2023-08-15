6441818a8702631796cb1e70c585893df9511739
Rafik006 <rafikmhadhbi01@gmail.com>
2023-08-15 15:50:13 +0100

    edit and delete blogs

--- dashboard/app/manageBlogs/page.jsx
+++ dashboard/app/manageBlogs/page.jsx
@@ -3,24 +3,30 @@ import React, { useEffect, useState } from 'react'
 import "./manageBlog.css"
 import BlogCard from '@/components/blogsCard/blogCard.jsx'
 import axios from "axios";
+
 const page = () => {
   const [blogs,setBlogs]=useState([])
+  const [refresh,setRefresh]=useState(false)
+ 
   useEffect(()=>{
     axios
-    .get("http://localhost:3001/blogs/getAll", { headers: { "Cache-Control": "no-store" } })
+    .get("http://localhost:3001/blogs/getAll",{cache:'no-store'})
     .then((response) => {
       setBlogs(response.data);
     })
     .catch((error) => {
       console.error("Error fetching blogs:", error);
     });
-  },[])
+  },[refresh])
+  const handleRefresh=()=>{
+    setRefresh(!refresh)
+   }
   return (
     <div className='manage-blog'>
        <h1 className="title">Gérer blog</h1>
        <div className="card-container">
         {
-          blogs.map(blog=><BlogCard blog={blog}/>)
+          blogs.map(blog=><BlogCard blog={blog} handleRefresh={handleRefresh}/>)
         }
        </div>
     </div>
--- dashboard/components/sidebar/sidebar.jsx
+++ dashboard/components/sidebar/sidebar.jsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
                 <li className='side-link'><span><FontAwesomeIcon icon={faBorderAll} style={{ width: "21px" }} /></span><a href="/allStudents" className='side-anchor-link'>Tous le demandes</a></li>
                 <li className='side-link'><span><FontAwesomeIcon icon={faInbox} style={{ width: "21px" }} /></span><a href="/" className='side-anchor-link'>Tous le messages</a></li>
                 <li className='side-link'><span><FontAwesomeIcon icon={faPeopleGroup} style={{ width: "21px" }} /></span><a href="/" className='side-anchor-link'>Cohorts</a></li>
-                <li className='side-link'><span><FontAwesomeIcon icon={faFeather} style={{ width: "21px" }} /></span><a href="/" className='side-anchor-link'>Blogs</a></li>
+                <li className='side-link'><span><FontAwesomeIcon icon={faFeather} style={{ width: "21px" }} /></span><a href="/addBlog" className='side-anchor-link'>Blogs</a></li>
                 <li className='side-link'><span><FontAwesomeIcon icon={faCalendar} style={{ width: "21px" }} /></span><a href="/" className='side-anchor-link'>Events</a></li>
                 <li className='side-link'><span><FontAwesomeIcon icon={faDiagramProject} style={{ width: "21px" }} /></span><a href="/" className='side-anchor-link'>
                     Projet récemment publié
